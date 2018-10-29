6ade8b538bb550298a4ff2e54cddbf62d2be6c63
JackoQm <jackoymqm@gmail.com>
2018-10-29 04:16:56 -0400

    Delete unnecessary files and add deploy script

---
+++ quick_deploy.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+usage() { 
+	echo "quick_deploy [-acpdh] [commit comment]"
+}
+commit() { 
+	git add . && git commit -m "$*" 
+}
+push() { 
+	git push origin hexo 
+}
+deploy() { 
+	git clean && git generate && git deploy 
+}
+case $# in
+  1)
+    case $1 in
+        "-p") push
+        ;;
+        "-d") deploy
+        ;;
+        "-pd") push && deploy
+        ;;
+        *) usage
+        ;;
+    esac
+  ;;
+  2)
+    case $1 in
+        "-c") commit "$2"
+        ;;
+        "-cp") commit "$2" && push
+        ;;
+        "-a") commit "$2" && push && deploy
+        ;;
+        *) usage
+        ;;
+    esac
+  ;;
+  *)
+  usage
+  ;;
+esac
